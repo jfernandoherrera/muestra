26741caf5b7e6c4bfa6626c66866564a3c1131e5
Jose Fernando Herrera <jfernandoherrera1@gmail.com>
2014-12-15 12:49:00 -0500

    fd

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -22,59 +22,9 @@ var app =angular.module('FCIWEB', []).run(['$window',
         }(document, 'script', 'facebook-jssdk'));
     }
 ]).config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/new',             { templateUrl: 'partials/new.html',  controller: loginCtrl }).
-            otherwise({ redirectTo: '/new' });
+        $routeProvider.when('/',             { templateUrl: '../index.html',  controller: 'loginCtrl' }).
+            otherwise({ redirectTo: '/' });
         }]);
 /*
 angular.module('FCIWEB');*/
-
-    function loginCtrl($scope,$http,$resource) {
-        $scope.logIn = function () {
-            FB.login(function (response) {
-                    if (response.authResponse) {
-                        console.log("dasers");
-                        $http.post('/login', {userId: response.authResponse.userID})
-                            .success(
-                            function (data, status) {
-
-//                                $window.location.href = '/home.html';
-                            }
-                        )
-                            .error(
-                            function (data) {
-                                if (data.err == "User not found.") {
-                                    FB.api(
-                                        "/me",
-                                        function (response1) {
-                                            if (response1 && !response1.error) {
-                                                var user = {
-                                                    userId: response1.id,
-                                                    cc: "11111",
-                                                    email: response1.email,
-                                                    name: response1.first_name
-                                                };
-                                                $http.post('/register', user)
-                                                    .success(
-                                                    function (data, status) {
-                                                        //$state.go('/list');
-//                                                            $window.location.href = '/home.html';
-                                                    }
-                                                )
-                                                    .error(
-                                                    function (data, status) {
-                                                    }
-                                                );
-                                            }
-                                        }
-                                    );
-                                }
-                            }
-                        );
-                    } else {
-                        console.log('User cancelled login or did not fully authorize.');
-                    }
-                },
-                {scope: 'email,user_friends'});
-        };
-    }
 
--- server.js
+++ server.js
@@ -3,17 +3,16 @@
  * Module dependencies.
  */
 
-var express = require('express')
-    , router = require('./router')
-    , user = require('./router/api')
+var router = require('./router/index')
+    ,express = require('express')
     , http = require('http')
     , path = require('path');
 
-var app = express();
+var myapp = express();
 var mongoose = require('mongoose');
 // all environments
 var dbHost = process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://localhost:27017/";
-var dbName = process.env.OPENSHIFT_APP_NAME       || "tripList";
+var dbName = process.env.OPENSHIFT_APP_NAME       || "FCIWEB";
 var dbUrl = "" + dbHost + dbName;
 
 var db = mongoose.createConnection(dbUrl, function(err) {
@@ -27,26 +26,25 @@ var db = mongoose.createConnection(dbUrl, function(err) {
 var ipaddr = process.env.OPENSHIFT_NODEJS_IP || "localhost";
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
-app.set('port', port);
-app.set('ipaddr', ipaddr);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+myapp.set('port', port);
+myapp.set('ipaddr', ipaddr);
+myapp.set('views', __dirname + '/public/views');
+myapp.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
+myapp.use(express.logger('dev'));
+myapp.use(express.bodyParser());
+//myapp.use(express.methodOverride());
 
-app.use(express.static(__dirname+ '/public'));
+http.createServer(myapp);
+myapp.use(express.static(__dirname+ '/public'));
 
 // development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
 //pp.render('index', {title: 'FCI WEB'});
 
+
 //app.get('/', router.index);
-router(app,db);
+router(myapp,db);
+//myapp.use(router);
 //
-http.createServer(app).listen(app.get('port'), app.get('ipaddr'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+myapp.listen(myapp.get('port'), myapp.get('ipaddr'), function(){
+  console.log('Express server listening on port ' + myapp.get('port'));
 });
