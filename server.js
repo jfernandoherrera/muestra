790f44ff1700eaf21260e6cdc3f40ce7bca72e00
Jose Fernando Herrera <jfernandoherrera1@gmail.com>
2014-12-18 15:26:02 -0500

    dets

--- server.js
+++ server.js
@@ -48,12 +48,12 @@ mongoClient.connect('mongodb://'+ connection_string,
 //myapp.use(router);
 //
         ippaddress= process.env.OPENSHIFT_NODEJS_IP;
-        if (typeof ipaddress === "undefined") {
+      /*  if (typeof ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
             console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
            ipaddress = "127.0.0.1";
-        };
+        };*/
 
         myapp.listen(process.env.OPENSHIFT_NODEJS_PORT ,ipaddress , function(){
             console.log('Express server listening on port ' +process.env.OPENSHIFT_NODEJS_PORT || 8888);
