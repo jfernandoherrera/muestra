5e47a3ff705439bfd3d3bbd6243d3fb449e26188
Jose Fernando Herrera <jfernandoherrera1@gmail.com>
2014-12-17 20:48:10 -0500

    fedD

--- server.js
+++ server.js
@@ -6,45 +6,46 @@
 var router = require('./router/index')
     ,express = require('express')
     , http = require('http')
+    , mongoClient = require('mongodb').MongoClient
     , path = require('path');
 
 var myapp = express();
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 // all environments
-var dbHost = process.env.HEROKU_MONGODB_DB_URL || "mongodb://localhost:27017/";
-var dbName = process.env.HEROKU_APP_NAME       || "FCIWEB";
-var dbUrl = "" + dbHost + dbName;
+mongoClient.connect('mongodb://'+ self.connection_string,
+    function(err, db){
+        if(err){
+            throw err;
+        }
+        console.log('DB up.');
 
-var db = mongoose.createConnection(dbUrl, function(err) {
-    if (err) {
-        console.log("[Error] MongoDB '" + dbUrl + "' connection failed : " + err);
-    }
-});
-//var reqHandler = new user(db);
-// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-// app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-//var ipaddr = process.env.HEROKU_NODEJS_IP || "localhost";
-var port = process.env.PORT || process.env.HEROKU_NODEJS_PORT || 8000;
-
-myapp.set('port', port);
+        myapp.set('port', process.env.PORT );
 //myapp.set('ipaddr', ipaddr);
-myapp.set('views', __dirname + '/public/views');
-myapp.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
-myapp.use(express.logger('dev'));
-myapp.use(express.bodyParser());
+        myapp.set('views', __dirname + '/public/views');
+        myapp.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
+        myapp.use(express.logger('dev'));
+        myapp.use(express.bodyParser());
 //myapp.use(express.methodOverride());
 
-http.createServer(myapp);
-myapp.use(express.static(__dirname+ '/public'));
+        http.createServer(myapp);
+        myapp.use(express.static(__dirname+ '/public'));
 
 // development only
 //myapp.render('index', {title: 'FCI WEB'});
 
 
 //app.get('/', router.index);
-router(myapp,db);
+        router(myapp,db);
 //myapp.use(router);
 //
-myapp.listen(myapp.get('port'), myapp.get('ipaddr'), function(){
-  console.log('Express server listening on port ' + myapp.get('port'));
-});
+        myapp.listen(myapp.get('port'), myapp.get('ipaddr'), function(){
+            console.log('Express server listening on port ' + myapp.get('port'));
+        });
+    }
+);
+//var reqHandler = new user(db);
+// app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+// app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+//var ipaddr = process.env.HEROKU_NODEJS_IP || "localhost";
+//var port = process.env.PORT || process.env.HEROKU_NODEJS_PORT || 8000;
+
